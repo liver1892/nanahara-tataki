e303ac7c69f7b151088d2cedb09d19276a0bd034
liver1892 <liverynwa1897@gmail.com>
2025-09-15 21:33:31 +0900

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ for (let i = 0; i < 10; i++) {
     const holeDiv = document.createElement('div');
     holeDiv.classList.add('hole');
     const moleImage = document.createElement('img');
-    moleImage.src = 'images/nanahara.png'; // 画像のパスを指定
+    moleImage.src = 'images/nanahara.png';
     moleImage.classList.add('mole');
     holeDiv.appendChild(moleImage);
     holes.appendChild(holeDiv);
@@ -28,7 +28,7 @@ function randomHole() {
     hole.classList.add('up');
     setTimeout(() => {
         hole.classList.remove('up');
-    }, 700); // 0.7秒後に隠れる
+    }, 700);
 }
 
 // ゲーム開始処理
@@ -38,10 +38,9 @@ function startGame() {
     scoreDisplay.textContent = score;
     timeDisplay.textContent = time;
 
-    // ゲームが開始したらボタンを非活性化
     startButton.disabled = true;
 
-    moleInterval = setInterval(randomHole, 1000); // 1秒ごとにモグラを出す
+    moleInterval = setInterval(randomHole, 1000);
     gameInterval = setInterval(() => {
         time--;
         timeDisplay.textContent = time;
@@ -49,10 +48,9 @@ function startGame() {
             clearInterval(moleInterval);
             clearInterval(gameInterval);
             alert('ゲーム終了！あなたの点数は' + score + '点です。');
-            // ゲーム終了したらボタンを再度活性化
             startButton.disabled = false;
         }
-    }, 1000); // 1秒ごとに時間を減らす
+    }, 1000);
 }
 
 // モグラを叩いた時の処理
@@ -63,11 +61,9 @@ allHoles.forEach(hole => {
             scoreDisplay.textContent = score;
             hole.classList.remove('up');
 
-            // 叩いた画像を一時的に非表示にする
             const moleImage = hole.querySelector('.mole');
             moleImage.style.visibility = 'hidden';
 
-            // 0.1秒後に再び表示する
             setTimeout(() => {
                 moleImage.style.visibility = 'visible';
             }, 100);
